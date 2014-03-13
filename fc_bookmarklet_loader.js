dd905dd97e06325c0f2d8c07a9e43a53c1acc74e
Bryanarby <just.me.Bry@gmail.com>
2014-03-13 12:37:32 +0100

    Update fc_bookmarklet_loader.js

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -2,7 +2,7 @@ javascript:(
   function () {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-	js.setAttribute('id', 'frozenCookieScript');
+    js.setAttribute('id', 'frozenCookieScript');
     js.setAttribute('src', 'https://raw.github.com/Bryanarby/FrozenCookies/master/frozen_cookies.js');
     document.head.appendChild(js);
  }()
