0fc6d97ff5944ff3cc311dc4cf9a4cd9b5c20105
Bryanarby <just.me.Bry@gmail.com>
2014-04-09 19:22:50 +0200

    quick patchup for 1.420

--- fc_main.js
+++ fc_main.js
@@ -88,7 +88,7 @@ function setOverrides() {
   Game.Draw = function() {if(document.hasFocus() && $('#statGraphContainer').is(':hidden')){Game.oldDraw();}}
 
   // Remove the following when turning on tooltop code
-  Game.RebuildStore();
+  Game.RefreshStore()
   Game.RebuildUpgrades();
   beautifyUpgradesAndAchievements();
   // Replace Game.Popup references with event logging
